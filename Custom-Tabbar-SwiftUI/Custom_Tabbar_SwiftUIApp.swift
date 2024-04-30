7a7de90708d08cb8e4c3e091181d638cf9b260cd
Caner Ates <cnrates@gmail.com>
2024-04-30 17:21:20 +0300

    Initial Commit

---
+++ Custom-Tabbar-SwiftUI/Custom_Tabbar_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_Tabbar_SwiftUIApp.swift
+//  Custom-Tabbar-SwiftUI
+//
+//  Created by Caner Ates on 30.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Custom_Tabbar_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
